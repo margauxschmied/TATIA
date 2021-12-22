2f42edf02ca1fa5aa07c6e1512ec2356dae3c3b6
margauxschmied <margaux.schmied@icloud.com>
2021-12-22 16:09:57 +0100

    graph d'accuracy

--- classifier.py
+++ classifier.py
@@ -1,3 +1,4 @@
+from os import error
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -40,6 +41,9 @@ from sklearn import metrics
 from sklearn.naive_bayes import CategoricalNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, ComplementNB, BernoulliNB, CategoricalNB
+from sklearn.utils import shuffle
+from matplotlib import pyplot as plt
+
 
 stopwords_list = stopwords.words('english')
 stopwords_list += list(string.punctuation)
@@ -84,20 +88,29 @@ def clean_text(text):
 classifier = None
 vectorizer = None
 
-def train(df):
+def train(df, test_size, train_size):
     global classifier, vectorizer
     print("Training...")
-    sentences = df["description"].values
+    """sentences = df["description"].values
     vectorizer = CountVectorizer(min_df=0, lowercase=False)
     vectorizer.fit(sentences)
     vectorizer.transform(sentences)
 
     X = df.drop("Unnamed: 0", axis=1)
     X = X.drop("genre", axis=1).drop("id", axis=1).drop("title", axis=1)
-    Y = df["genre"].values
+    Y = df["genre"].values"""
+
+    sentences = shuffle(df["description"].values, n_samples=2000)
+    vectorizer = CountVectorizer(min_df=0, lowercase=False)
+    vectorizer.fit(sentences)
+    vectorizer.transform(sentences)
+
+    X = df.drop("Unnamed: 0", axis=1).drop("genre", axis=1).drop("id", axis=1).drop("title", axis=1)
+    X = shuffle(X, n_samples=2000)
+    Y = shuffle(df["genre"], n_samples=2000)
 
 
-    sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, Y, test_size=1/3, train_size=2/3)#, random_state=50
+    sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, Y, test_size=test_size, train_size=train_size)#, random_state=50
     vectorizer = CountVectorizer()
     vectorizer.fit(sentences_train)
 
@@ -134,7 +147,7 @@ def train(df):
 
 
 
-def accuracy(df):
+def predictError(df):
     genre = df["genre"]
     genre_predicted = df["genre_predit"]
     length_genre = len(genre)
@@ -144,6 +157,7 @@ def accuracy(df):
             error += 1
 
     print(f"Error : {(error/length_genre)*100:.3f}%")
+    return (error/length_genre)*100
     
 
 
@@ -163,7 +177,7 @@ def predict(file_path, output, df_trained, df_to_predict):
     # df2 = pd.DataFrame(data={"title": df_to_predict["title"].values, "genre": df_trained["genre"], "genre_predit": classified})
     df2 = pd.DataFrame(data={"title": df_to_predict["title"].values, "genre": df_to_predict["genre"], "genre_predit": classified})
 
-    accuracy(df2)
+    predictError(df2)
     df2.to_csv(output)
 
 def convert_string_to_dataset_prediction(title, description, genre_attendu):
@@ -215,7 +229,111 @@ def keras(df):
                 loss='binary_crossentropy',
                 metrics=['accuracy'])
     model.summary()
-    
+
+def graphAccuracy(df_trained_data, df_to_predict):
+    test_sizes=[x/10 for x in range(1, 10)]
+    train_sizes=[x/10 for x in range(1, 10)]
+
+    x_labels = [0]
+    x_ticks=[0]
+    i=0
+    for a in test_sizes:
+        for b in train_sizes:
+            x_labels.append("("+str(a)+","+str(b)+")")
+            i+=1
+            x_ticks.append(i)
+    i=1
+    plt.xticks(ticks=x_ticks, labels=x_labels)
+    x = []
+    y = []
+
+    nbOfGraph=0
+    for test_size in test_sizes:
+        for train_size in train_sizes:
+            if test_size+train_size<1:
+                print(str(test_size)+" "+str(train_size))
+                print(x_ticks[i])
+
+                score = train(df_trained_data, test_size, train_size)
+                y.append(score)
+                x.append(x_ticks[i])
+                predict("archive/dataset_csv/test_data.csv", "predicted.csv", df_trained_data, df_to_predict)
+                print("Accuracy:", score)
+                
+            else:
+                y.append(0)
+                x.append(x_ticks[i])
+
+            if i!=0 and i%9 ==0:
+                    plt.scatter(x, y)
+
+                    plt.savefig("variation_accuracy"+str(nbOfGraph)+".png")
+                    x = []
+                    y = []
+                    plt.clf()
+                    plt.xticks(ticks=x_ticks, labels=x_labels)
+
+                    nbOfGraph+=1
+            i+=1
+
+
+    #plt.scatter(x, y)    
+
+    #plt.savefig("variation_accuracy.png")
+
+def graphError(df_trained_data, df_to_predict):
+    test_sizes=[x/10 for x in range(1, 10)]
+    train_sizes=[x/10 for x in range(1, 10)]
+
+    x_labels = [0]
+    x_ticks=[0]
+    i=0
+    for a in test_sizes:
+        for b in train_sizes:
+            x_labels.append("("+str(a)+","+str(b)+")")
+            i+=1
+            x_ticks.append(i)
+    i=1
+    plt.xticks(ticks=x_ticks, labels=x_labels)
+    x = []
+    y = []
+
+    nbOfGraph=0
+    for test_size in test_sizes:
+        for train_size in train_sizes:
+            if test_size+train_size<1:
+                print(str(test_size)+" "+str(train_size))
+                print(x_ticks[i])
+
+                score = train(df_trained_data, test_size, train_size)
+                error=predictError(df)
+
+                y.append(error)
+                x.append(x_ticks[i])
+                predict("archive/dataset_csv/test_data.csv", "predicted.csv", df_trained_data, df_to_predict)
+                print("Accuracy:", score)
+                
+            else:
+                y.append(0)
+                x.append(x_ticks[i])
+
+            if i!=0 and i%9 ==0:
+                    plt.scatter(x, y)
+
+                    plt.savefig("variation_error"+str(nbOfGraph)+".png")
+                    x = []
+                    y = []
+                    plt.clf()
+                    plt.xticks(ticks=x_ticks, labels=x_labels)
+
+                    nbOfGraph+=1
+            i+=1
+
+
+    #plt.scatter(x, y)    
+
+    #plt.savefig("variation_accuracy.png")
+
 
 
 if __name__ == "__main__":
@@ -224,10 +342,12 @@ if __name__ == "__main__":
     #keras(df_trained_data)
     df=convert_string_to_dataset_prediction("les tuches 4", "Twenty-five years after the original series of murders in Woodsboro, a new killer emerges, and Sidney Prescott must return to uncover the truth.", "horror")
     df_to_predict = pd.read_csv("archive/dataset_csv/test_data_solution_clean.csv")
-    score = train(df_trained_data)
-    predict("archive/dataset_csv/test_data.csv", "predicted.csv", df_trained_data, df_to_predict)
-    print("Accuracy:", score)
+    graphError(df_trained_data, df_to_predict)
+    #score = train(df_trained_data, 2/3, 1/3)
 
+    #predict("archive/dataset_csv/test_data.csv", "predicted.csv", df_trained_data, df_to_predict)
+    #print("Accuracy:", score)
+    
 
 # X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=1/3)
 
